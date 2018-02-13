c5e4f055baf443f83cc07889684fff6997962f43
RChastel <rchastel@protonmail.com>
2018-02-13 14:08:40 +0100

    Update test suite

--- tests/__init__.py
+++ tests/__init__.py
@@ -26,10 +26,58 @@ import doctest
 import unittest
 import importlib
 
+from gnuplotting.variable import Namespace
+
 TESTS_ROOT = os.path.abspath(os.path.dirname(__file__))
 ROOT = os.path.dirname(TESTS_ROOT)
 
-            
+class TestNamespace(Namespace, unittest.TestSuite):
+    """A testsuite where subtests are reachable from attributes
+
+    ex:
+       self.process : the process module test suite
+       self.process.GnuplotProcess : the GnuplotProcess class test suite
+       self.process.GnuplotProcess.wait : the wait method test suite
+    """
+    def __init__(self, rel_path):
+        super(TestNamespace, self).__init__(__rel_path=rel_path)
+
+    
+    def addTest(self, test):
+        prefix_len = len(self.__rel_path)+1
+        test_id = test.id()
+        test_rel_id = test_id[prefix_len:]
+        id_parts = test_rel_id.split('.')
+        id_prefix = id_parts[0]
+        if id_prefix in self:
+            self[id_prefix].addTest(test)
+        elif id_prefix == test_rel_id:
+            self[id_prefix] = unittest.TestSuite([test])
+        else:
+            self[id_prefix] = TestNamespace(test_id[:prefix_len+len(id_prefix)])
+            self[id_prefix].addTest(test)
+
+    def addTests(self, tests):
+        for test in tests:
+            self.addTest(test)
+
+    def countTestCases(self):
+        acc = 0
+        for k, v in self.items():
+            if isinstance(v, TestNamespace):
+                acc += v.countTestCases()
+            else:
+                acc += 1
+        return acc
+
+    def run(self, result):
+        for k, v in self.items():
+            v.run(result)
+        return result
+
+    def debug(self):
+        for k, v in self.items():
+            v.debug()
 
 def load_tests(loader=None, test=None, pattern=None):
     if not ROOT in sys.path:
@@ -44,14 +92,12 @@ def load_tests(loader=None, test=None, pattern=None):
     test_modules = ((mod_name,
                      importlib.import_module(mod_path, gnuplotting)) \
                         for mod_name, mod_path in test_module_path_by_names)
-    tests = {test_mod_name: doctest.DocTestSuite(module=test_mod) \
-             for test_mod_name, test_mod in test_modules \
-             if inspect.getsource(test_mod).find('"""') > -1}
-    doctests_dict = {name: unittest.TestSuite([test]) \
-                     for name, test in tests.items()}
-    doctests_dict['alltests'] = unittest.TestSuite(tests.values())
-    doctests = type('Doctests', (unittest.TestSuite,), doctests_dict)()
-    doctests.addTest(doctests_dict['alltests'])
+    test_modules = ((mod_name, mod) for mod_name, mod in test_modules \
+                    if inspect.getsource(mod).find('"""') > -1)
+    doctests = TestNamespace(gnuplotting)
+    for name, test_mod in test_modules:
+        test_suite = doctest.DocTestSuite(module=test_mod)
+        doctests.addTests(test_suite)
     return doctests
 
 doctests = load_tests()
